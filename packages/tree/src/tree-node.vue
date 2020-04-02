1e1aabbbe3e6f2321a78ff99c5f2fcbf56fd5d53
Mahmoud Seifeldeen <seifodev@gmail.com>
2020-04-02 07:04:23 +0200

    Update tree-node.vue

--- packages/tree/src/tree-node.vue
+++ packages/tree/src/tree-node.vue
@@ -24,7 +24,7 @@
     ref="node"
   >
     <div class="el-tree-node__content"
-      :style="{ 'padding-left': (node.level - 1) * tree.indent + 'px' }">
+      :style="style">
       <span
         @click.stop="handleExpandIconClick"
         :class="[
